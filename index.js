342854e9bb3eb36bd07675ef6acb952fc22591d9
HeEwOn96 <gjds2361@gmail.com>
2022-07-16 19:03:16 +0900

    첫번째 학습

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://heewon:6j9u2n4g@bilerplate.rnt0q.mongodb.net/?retryWrites=true&w=majority',{
+}).then(() => console.log('mongoDB Connected....'))
+.catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
